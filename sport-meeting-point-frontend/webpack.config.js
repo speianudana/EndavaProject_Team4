09c9743cf3b02249d3a86c7429e003b9bb71fc45
Ivan <ipostu20000127@gmail.com>
2019-11-08 23:54:34 +0200

    simple webpack config for frontend project

---
+++ sport-meeting-point-frontend/webpack.config.js
@@ -0,0 +1,110 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack')
+
+module.exports = (env, options) => {
+    const isDev = options.mode === 'development'
+    console.log('mode: ' + options.mode)
+    console.log()
+
+    return {
+        entry: isDev ? [
+            'react-dev-utils/webpackHotDevClient',
+            './src/main/index.js'
+        ] : ['./src/main/index.js'],
+        output: {
+            path: path.join(__dirname, './build'),
+            filename: isDev ? '[name].dev.js' : '[name].[contenthash].js',
+            chunkFilename: '[name]_[chunkhash].js',
+        },
+        devtool: isDev ? 'inline-source-map' : '',
+        devServer: isDev ? {
+            contentBase: "/build/client",
+            overlay: true,
+            // host: '192.168.1.5',
+        } : {},
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.(scss|sass|css)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    mode: 'local',
+                                    localIdentName: '[path]_[name]_[local]'
+                                }
+                            }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        },
+                    ]
+                },
+                {
+                    test: /\.(png|jpe?g|gif|ico)$/i,
+                    exclude: /node_modules/,
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'images',
+                        name(file) {
+                            if (isDev) {
+                                return '[path][name].[ext]';
+                            }
+
+                            return '[contenthash].[ext]';
+                        },
+                    },
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/main/index.html',
+                filename: 'index.html',
+                // favicon: './favicon.ico'
+            }),
+
+            ...(isDev ? [] : [
+                new OptimizeCssAssetsPlugin({}),
+                new MiniCssExtractPlugin({
+                    filename: isDev ? '[name].css' : '[name].[contenthash].css',
+                    chunkFilename: isDev ? '[id].css' : '[id].[contenthash].css',
+                }),
+                new CleanWebpackPlugin(),
+                new webpack.HashedModuleIdsPlugin(),
+            ]),
+        ],
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: 'vendors',
+                        chunks: 'all'
+                    }
+                }
+            }
+
+        }
+
+
+    }
+
+}
