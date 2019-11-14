457ea81f3d6f14e3a59db8e114b35a351da9cada
Ivan <ipostu20000127@gmail.com>
2019-11-14 02:09:54 +0200

    #

--- src/main/java/com/webapp/sportmeetingpoint/SportMeetingPointApplication.java
+++ src/main/java/com/webapp/sportmeetingpoint/SportMeetingPointApplication.java
@@ -4,12 +4,15 @@ package com.webapp.sportmeetingpoint;
 import com.webapp.sportmeetingpoint.domain.entities.AppUserRoles;
 import com.webapp.sportmeetingpoint.domain.entities.UserRole;
 import com.webapp.sportmeetingpoint.persistance.UserRoleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
 
+@Slf4j
 @SpringBootApplication
 public class SportMeetingPointApplication {
 
@@ -27,7 +30,9 @@ public class SportMeetingPointApplication {
 				userRoleRepository.save(new UserRole(role.ordinal(), role.toString(), null));
 			}
 
-
+			for	(AppUserRoles role : AppUserRoles.values()){
+				List<UserRole> q = userRoleRepository.findAllByName(role.toString());
+			}
 
 		};
 	}
--- src/main/java/com/webapp/sportmeetingpoint/persistance/UserRoleRepository.java
+++ src/main/java/com/webapp/sportmeetingpoint/persistance/UserRoleRepository.java
@@ -3,10 +3,14 @@ package com.webapp.sportmeetingpoint.persistance;
 import com.webapp.sportmeetingpoint.domain.entities.UserRole;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRoleRepository extends CrudRepository<UserRole, Integer> {
 
 
   void deleteAll();
   UserRole save(UserRole u);
+  List<UserRole> findAllByName(String name);
+
 
 }
