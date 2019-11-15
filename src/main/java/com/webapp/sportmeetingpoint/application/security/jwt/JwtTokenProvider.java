93d13c626a15a086bf9f1239894ef8399058da76
Ivan <ipostu20000127@gmail.com>
2019-11-15 02:25:43 +0200

    #

--- src/main/java/com/webapp/sportmeetingpoint/application/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/webapp/sportmeetingpoint/application/security/jwt/JwtTokenProvider.java
@@ -2,11 +2,22 @@ package com.webapp.sportmeetingpoint.application.security.jwt;
 
 
 import com.webapp.sportmeetingpoint.domain.entities.UserRole;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -18,20 +29,65 @@ public class JwtTokenProvider {
   @Value("${jwt.token.expired}")
   private Long validityInMilliseconds;
 
+  @Autowired
+  UserDetailsService userDetailsService;
+
+  @PostConstruct
+  protected void init(){
+    secret = Base64.getEncoder().encodeToString(secret.getBytes());
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder(){
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder;
+  }
+
   public String createToken(String userName, List<UserRole> userRoles){
-    return null;
+    Claims claims =  Jwts.claims().setSubject(userName);
+    claims.put("roles", getRolenames(userRoles));
+
+    Date now = new Date();
+    Date validity = new Date(now.getTime()+validityInMilliseconds);
+
+    return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .signWith(SignatureAlgorithm.HS256, secret)
+            .compact();
   }
 
   public Authentication getAuthentication (String token){
-    return null;
+    UserDetails userDetails = this.userDetailsService.loadUserByUsername(getUserName(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
   }
 
   public String getUserName(String token){
+    return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject() ;
+  }
+
+  public String resolveToken(HttpServletRequest req){
+    String bearerToken = req.getHeader("Authorization");
+    if(bearerToken!=null && bearerToken.startsWith("Bearer_")){
+      return bearerToken.substring(7, bearerToken.length());
+    }
+
     return null;
   }
 
   public boolean validateToken(String token){
-    return true;
+    try{
+      Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+
+      if(claims.getBody().getExpiration().before(new Date())){
+        return false;
+      }
+
+      return true;
+    }catch(JwtException | IllegalArgumentException e){
+      throw new JwtAuthenticationException("JWT token is expired or invalid!");
+    }
   }
 
   private List<String> getRolenames(List<UserRole> userRoles){
