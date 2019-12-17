dddfd5d1693a17570d7f95c15cea8e0ac009e536
Ivan <ipostu20000127@gmail.com>
2019-12-17 02:45:39 +0200

    VERY SERIOUS PROBLEM SOLVED

--- src/main/java/com/webapp/sportmeetingpoint/application/config/SecurityConfig.java
+++ src/main/java/com/webapp/sportmeetingpoint/application/config/SecurityConfig.java
@@ -6,12 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final JwtTokenProvider jwtTokenProvider;
@@ -36,19 +43,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-            .httpBasic().disable()
-            .csrf().disable()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .cors()
             .and()
+            .csrf().disable()
             .authorizeRequests()
             .antMatchers(AUTH_ENDPOINT).permitAll()
-//            .antMatchers("/api/**").permitAll()
+            .antMatchers("/api/user_personal_data/get_data").permitAll()
             .antMatchers(EVENT_ENDPOINT).hasRole("USER")
             .anyRequest().authenticated()
             .and()
             .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
             .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            .antMatchers("/api/**").permitAll()
+            .and()
             .apply(new JwtSecurityConfigurer(jwtTokenProvider));
   }
+  
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+    
+    return source;
+  }
+  
+  
 }
 
