8a3f5560958db396263e1a2114570436dc3c04ab
Ivan <ipostu20000127@gmail.com>
2019-12-22 22:36:48 +0200

    Event component downloads images from the backend

--- sport-meeting-point-frontend/src/components/Pages/Home/Index/Article/Article.statefull.jsx
+++ sport-meeting-point-frontend/src/components/Pages/Home/Index/Article/Article.statefull.jsx
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types'
 import { post } from 'axios'
 import { url } from '../../../../../utils/server-url'
 
+const thumbnailSrc = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22508%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20508%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16544932167%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16544932167%22%3E%3Crect%20width%3D%22508%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22169.7578125%22%20y%3D%22123.9%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+
+
 class ArticleStatefull extends Component {
 
   constructor(props) {
@@ -20,20 +23,25 @@ class ArticleStatefull extends Component {
 
     const self = this
 
-    post(`${url}/api/event/image_by_id`, 15, {
+    post(`${url}/api/event/image_by_id`, 16, {
       headers: {
         'Content-Type': 'application/json',
       }
     })
       .then(result => {
 
-        if (self._isMounted)
-          // self.setState({
-          //   image: "data:image/png;base64," +
-          //     btoa(String.fromCharCode.apply(null, new Uint8Array(result.data.image)))
-          // })
-          // result.data.image
-          console.log(result.data)
+        if (self._isMounted) {
+
+          let TYPED_ARRAY = new Uint8Array(result.data.image);
+
+          let str = TYPED_ARRAY.reduce((acc, cur) => acc + String.fromCharCode(cur), '')
+          let base64String = btoa(str);
+          let imgData = 'data:image/jpg;base64,' + base64String
+
+          self.setState({
+            image: imgData
+          })
+        }
       })
 
   }
