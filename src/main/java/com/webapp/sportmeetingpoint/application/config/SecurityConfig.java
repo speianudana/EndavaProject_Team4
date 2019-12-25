7844fda7d75b7c66df505ebbbb146fa9d531568c
Ivan <ipostu20000127@gmail.com>
2019-12-25 21:24:34 +0200

    #

--- src/main/java/com/webapp/sportmeetingpoint/application/config/SecurityConfig.java
+++ src/main/java/com/webapp/sportmeetingpoint/application/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.webapp.sportmeetingpoint.application.config;
 import com.webapp.sportmeetingpoint.application.security.jwt.JwtSecurityConfigurer;
 import com.webapp.sportmeetingpoint.application.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -22,37 +23,40 @@ import javax.servlet.http.HttpServletResponse;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-  
+
   private final JwtTokenProvider jwtTokenProvider;
-  
+
   private static final String AUTH_ENDPOINT = "/api/auth/**";
   private static final String EVENT_ENDPOINT = "/api/event/**";
-  
+
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-  
+
   @Autowired
   public SecurityConfig(JwtTokenProvider jwtTokenProvider, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
     this.jwtTokenProvider = jwtTokenProvider;
     this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
   }
-  
+
   @Bean
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
   }
-  
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
+
       .cors()
       .and()
       .csrf().disable()
       .authorizeRequests()
+//      .antMatchers("/resources/**").permitAll()
       .antMatchers(AUTH_ENDPOINT).permitAll()
       .antMatchers("/api/event/add").hasRole("USER")
       .antMatchers("/api/event/all_events").permitAll()
       .antMatchers("/api/event/image_by_id").permitAll()
+      .antMatchers("/index.html", "/","/*.js","/*.css", "/images/**").permitAll()
       .anyRequest().authenticated()
       .and()
       .exceptionHandling()
@@ -63,16 +67,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and()
       .apply(new JwtSecurityConfigurer(jwtTokenProvider));
+
+//      http.authorizeRequests().antMatchers("/css/**", "/js/**", "/images/**").permitAll();
   }
-  
+
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-    
+
     return source;
   }
-  
-  
+
+
 }
 
